a0c86a386598809f33291d483d5aec27ffb4c32b
Kabir Tamari <rabbit.bonex@gmail.com>
2019-05-15 00:14:10 -0700

    added sending fucntion for menu

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Menu
+import json
 
 def home(request):
-	all_items= Menu.objects.all
-	return render(request, 'home.html', {'all_items': all_items})
+	if request.method == "POST":
+		json_str = request.body.decode(encoding= 'UTF-8')
+		print(json_str)
+	else:
+		all_items= Menu.objects.all
+		return render(request, 'home.html', {'all_items': all_items})
